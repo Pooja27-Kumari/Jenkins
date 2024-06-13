0c0c0aa454c56b06ff3d910d77541bff257e6199
prachi <prachi@prachi>
2024-06-14 01:13:25 +0530

    First commited

---
+++ demoJenkinProject/src/test/java/jenkinProject/Example.java
@@ -0,0 +1,12 @@
+package jenkinProject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Example {
+
+@Test
+public void demoJenkins() {
+	Reporter.log("demoJenkins is added", true);
+}
+}
