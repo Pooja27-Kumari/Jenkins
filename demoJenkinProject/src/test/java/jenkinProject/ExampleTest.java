b4f93d5879c182f6a0cbe16fcbcd212d32a82a1e
prachi <prachi@prachi>
2024-06-14 08:22:54 +0530

    commited

--- demoJenkinProject/src/test/java/jenkinProject/ExampleTest.java
+++ demoJenkinProject/src/test/java/jenkinProject/ExampleTest.java
@@ -3,10 +3,11 @@ package jenkinProject;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Example {
+public class ExampleTest {
 
 @Test
 public void demoJenkins() {
 	Reporter.log("demoJenkins is added", true);
+	System.out.println("life is unexpected");
 }
 }
